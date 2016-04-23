3bdbaa210a2ee82010b1bf1f17282d88c85ab4f2
Andriux5 <andriux959@gmail.com>
2016-04-23 21:08:27 +0300

    Added basic log in code

--- steamprofile/script.sh
+++ steamprofile/script.sh
@@ -1,1 +1,16 @@
-echo test
+clear
+
+#First startup screen
+echo "--- Steam Profile Picture Bot ---"
+echo "Please enter username and password."
+
+#Gets user info
+read -p "Username: " username
+read -s -p "Password: " password
+
+#Logs into Steam
+printf "\n\nLogging in..."
+$loginURL = "https://steamcommunity.com/login/home/index.php"
+
+
+wget --save-cookies cookies.txt --keep-session-cookies --post-data="username="+$username+"&password="+$password $loginURL
